cc01b206da73591e7daab71fd20a2e0c21ae2f54
jieunsse <jieunsse@gmail.com>
2025-12-03 15:29:18 +0900

    chore: 함수 형식 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import Header from '@src/components/header/Header';
-import Footer from '@src/components/footer/Footer';
-import Sidebar from '@src/components/sidebar/Sidebar';
+import { Header } from '@src/components/header/Header';
+import { Footer } from '@src/components/footer/Footer';
+import { Sidebar } from '@src/components/sidebar/Sidebar';
 
 export default function App() {
   return (
