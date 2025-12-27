076d2cdaeefefae9fda1bd19722419b804cb8664
이정원 <jwon133@gmail.com>
2025-12-27 13:55:13 +0900

    feat: card/detail ui 구현

--- src/components/card/CardDetail.tsx
+++ src/components/card/CardDetail.tsx
@@ -3,24 +3,23 @@ import { Card } from './Card';
 import wine2 from '@shared/assets/images/wine2.svg';
 import clsx from 'clsx';
 import { Chip } from './CardChip';
-import kebab from '@shared/assets/images/kebab.svg';
 
-interface CardMylistProps extends BaseCardProps {
+interface CardDetailProps extends BaseCardProps {
   price?: number;
 }
 
-export function CardMylist({ size = 'large', text = 'Detail', price = 10000 }: CardMylistProps) {
+export function CardDetail({ size = 'large', text = 'Detail', price = 10000 }: CardDetailProps) {
   // size에 따른 스타일 결정
   const cardSizeClass = {
     small: 'w-[343px] h-[164px] px-[20px] py-[16px] pb-[0px]',
-    medium: 'w-[704px] h-[228px] px-[24px] py-[20px] pb-[0px]',
-    large: 'w-[800px] h-[228px] px-[30px] py-[24px] pb-[0px]',
+    medium: 'w-[704px] h-[228px] pl-[60px] py-[20px] pb-[0px]',
+    large: 'w-[800px] h-[228px] pl-[100px] py-[24px] pb-[0px]',
   }[size || 'large'];
 
   const imageSizeClass = {
     small: 'w-[53px] h-[185px]',
     medium: 'w-[76px] h-[270px] ',
-    large: 'w-[76px] h-[270px] ',
+    large: 'w-[53px] h-[185px] ',
   }[size || 'large'];
 
   const imagePositionSizeClass = {
@@ -75,13 +74,6 @@ export function CardMylist({ size = 'large', text = 'Detail', price = 10000 }: C
           <Chip className={clsx(chipClass)}>₩ {price?.toLocaleString()}</Chip>
         </Card.Body>
       </Card.Container>
-      <Card.Icon>
-        <Card.Image
-          src={kebab}
-          alt="kebab"
-          className="h-[26px] w-[26px] cursor-pointer transition-opacity duration-300 hover:opacity-80"
-        />
-      </Card.Icon>
     </Card>
   );
 }
