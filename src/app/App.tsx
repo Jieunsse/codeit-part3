63850f4d72a4205934009203211f85d7ac2e8c41
이정원 <jwon133@gmail.com>
2025-12-27 13:19:29 +0900

    fix: 리뷰 수정 사항 반영 App.tsx 테스트 코드 삭제

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '@src/components/header/Header';
-import { CardMonthly } from '@src/components/card/CardMonthly';
-import { CardMylist } from '@src/components/card/CardMylist';
+
 export default function App() {
   return (
     <>
       <Header />
       <main>
         <Outlet />
-        <CardMonthly
-          size="large"
-          rating={4.8}
-          text="Ciel du Cheval Vineyard Collaboration Series II 2012"
-        />
-        <CardMonthly size="small" rating={4.5} text="Sentinel Carbernet Sauvignon 2016" />
-        <CardMylist
-          size="large"
-          rating={4.8}
-          text="Ciel du Cheval Vineyard Collaboration Series II 2012"
-          price={30000}
-        />
-        <CardMylist
-          size="small"
-          rating={4.5}
-          text="Sentinel Carbernet Sauvignon 2016"
-          price={20000}
-        />
-        <CardMylist
-          size="medium"
-          rating={4.8}
-          text="Napa Valley Cabernet Sauvignon 2017"
-          price={40000}
-        />
       </main>
     </>
   );
