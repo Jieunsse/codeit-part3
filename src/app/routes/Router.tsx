d3a4ac30965e649b44849fe4a2d7b28823e98121
wjdthdms <soeun1327@gmail.com>
2025-12-29 17:25:59 +0900

    chore: 테스트 페이지 삭제

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@app/App';
 import { Home } from '@pages/home/Home';
-import { InputTest } from '@src/pages/test/InputTest';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [{ index: true, element: <Home /> }],
-  },
-  {
-    path: '/inputTest',
-    element: <InputTest />,
   },
 ]);
