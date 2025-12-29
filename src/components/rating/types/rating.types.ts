b29069c5a44a76aa98dd439a7bb562e3d53a27aa
jieunsse <jieunsse@gmail.com>
2025-12-29 16:30:07 +0900

    feat: rating 필터 구현

--- src/components/rating/types/rating.types.ts
+++ src/components/rating/types/rating.types.ts
@@ -1,5 +1,5 @@
 export type RatingRange = 'all' | '4.8-5.0' | '4.5-4.8' | '4.0-4.5' | '3.0-4.0';
 
 export interface RatingFilterProps {
-  onClick: (rating: RatingRange) => void;
+  onSelect: (rating: RatingRange) => void;
 }
