cf65e80821369953c70eceeb5a822d1f08d4e565
jieunsse <jieunsse@gmail.com>
2025-12-31 21:20:45 +0900

    chore: 불필요한 테스트 파일 삭제

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import { DefaultLayout } from '../layouts/DefaultLayout';
 import { Home } from '@src/pages/home/Home';
 import { AuthLayout } from '../layouts/AuthLayout';
-import { TestTwo } from '@src/shared/test/TestTwo';
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +10,6 @@ export const router = createBrowserRouter([
   },
   {
     element: <AuthLayout />,
-    children: [{ path: '/test2', element: <TestTwo /> }],
+    children: [],
   },
 ]);
